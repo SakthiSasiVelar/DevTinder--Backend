b898eb6fea71c6fd6a6c112148e71bcf0dcba472
SakthiSasiVelar <sakthisasivelar@gmail.com>
2025-01-25 15:32:23 +0530

    learned and implemented routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
-const express = require('express');
+const express = require('express'); 
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("Testing route is called");
+app.use("/test",(req,res,next)=>{
+    console.log('use is called');
+    next();
 });
+
+app.all("/test",(req,res,next) =>{
+    console.log('all is called');
+    next();
+})
+
+app.get("/data",(req,res)=>{
+    res.send('data is send');
+})
+
+app.get("/test/123",(req,res)=>{
+    res.send('test route is called')
+})
 
 app.listen(3000 , () => {
     console.log('server running in the port 3000');
